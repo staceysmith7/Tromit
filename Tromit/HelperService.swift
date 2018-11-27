a58accc8fde6b1b5fe5dfabdc23a410856b2ffc0
staceysmith7 <staceykai.smith@gmail.com>
2018-11-27 12:26:54 +0200

    code refactor

---
+++ Tromit/HelperService.swift
@@ -0,0 +1,65 @@
+//
+//  HelperService.swift
+//  Tromit
+//
+//  Created by Stacey Smith on 11/27/18.
+//  Copyright © 2018 Devstek. All rights reserved.
+//
+
+import Foundation
+import  FirebaseStorage
+import FirebaseAuth
+
+class HelperService {
+    static func uploadDataToServer(data: Data, caption: String, onSuccess:  @escaping () -> Void) {
+          let photoIdString = NSUUID().uuidString
+        let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOT_REF).child("posts").child(photoIdString)
+        
+        storageRef.putData(data, metadata: nil) { (metadata, error) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            storageRef.downloadURL(completion: { ( url, error ) in
+                
+                if error != nil {
+                    
+                    return
+                }
+                
+                if let photoUrl = url?.absoluteString {
+                    self.sendDataToDatabase(photoUrl: photoUrl, caption:caption, onSuccess: onSuccess)
+                    
+                }
+            })
+        }
+    }
+    static func  sendDataToDatabase(photoUrl: String, caption: String, onSuccess: @escaping () -> Void) {
+        let newPostId = Api.Post.REF_POSTS.childByAutoId().key
+        let newPostReference = Api.Post.REF_POSTS.child(newPostId!)
+        
+        guard let currentUser = Auth.auth().currentUser else {
+            return
+        }
+        let currentUserId = currentUser.uid
+        newPostReference.setValue(["uid": currentUserId, "photoUrl": photoUrl, "caption": caption], withCompletionBlock: {
+            (error, ref) in
+            
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            
+            let myPostRef = Api.MyPosts.REF_MYPOSTS.child(currentUserId).child((newPostId!))
+            myPostRef.setValue(true, withCompletionBlock: { (error, ref) in
+                if error != nil {
+                    ProgressHUD.showError(error!.localizedDescription)
+                }
+            })
+            
+            ProgressHUD.showSuccess("Success")
+           onSuccess()
+        })
+        
+    }
+}
--- Tromit/PostApi.swift
+++ Tromit/PostApi.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import FirebaseDatabase
+import FirebaseAuth
 
 class PostApi {
     
@@ -31,5 +32,50 @@ class PostApi {
                 completion(post)
             }
         })
+    }
+    
+   func observeLikeCount(withPostId id: String, completion:
+    @escaping (Int) -> Void) {
+    REF_POSTS.child(id).observe(.childChanged, with: {
+        snapshot in
+        if let value = snapshot.value as? Int {
+            completion(value)
+            
+    }
+})
+}
+    func incrementLikes(postId: String, onSuccess: @escaping (Post) -> Void, onError: @escaping (_ errorMessage: String?) -> Void) {
+        let postRef = Api.Post.REF_POSTS.child(postId)
+       postRef.runTransactionBlock({ (currentData: MutableData) -> TransactionResult in
+            if var post = currentData.value as? [String : AnyObject], let uid = Auth.auth().currentUser?.uid {
+                
+                var likes: Dictionary<String, Bool>
+                likes = post["likes"] as? [String : Bool] ?? [:]
+                var likeCount = post["likeCount"] as? Int ?? 0
+                if let _ = likes[uid] {
+                    likeCount -= 1
+                    likes.removeValue(forKey: uid)
+                } else {
+                    likeCount += 1
+                    likes[uid] = true
+                }
+                post["likeCount"] = likeCount as AnyObject?
+                post["likes"] = likes as AnyObject?
+                
+                currentData.value = post
+                
+                return TransactionResult.success(withValue: currentData)
+            }
+            return TransactionResult.success(withValue: currentData)
+        }) { (error, committed, snapshot) in
+            if let error = error {
+                onError(error.localizedDescription)
+            }
+            if let dict = snapshot?.value as? [String: Any] {
+                let post = Post.transformPostPhoto(dict: dict, key: snapshot!.key)
+                onSuccess(post)
+             
+            }
+        }
     }
 }
