8a6a00de86cb561f1746bbdd2fa651f0f66de283
George Nhari <cassenhari@gmail.com>
2018-11-28 06:50:29 -0800

    space

--- Tromit/ViewControllers/SearchViewController.swift
+++ Tromit/ViewControllers/SearchViewController.swift
@@ -24,7 +24,7 @@ class SearchViewController: UIViewController {
         let searchItem = UIBarButtonItem(customView: searchBar)
         self.navigationItem.rightBarButtonItem = searchItem
         
-        self.users.removeAll()
+        self.users.removeAll()  
     }
     
     func doSearch() {
