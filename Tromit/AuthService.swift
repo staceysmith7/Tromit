3c3c7f354e6427b76a8a1d1a162ab720af77adb4
staceysmith7 <staceykai.smith@gmail.com>
2018-11-29 10:43:47 +0200

    trial

--- Tromit/AuthService.swift
+++ Tromit/AuthService.swift
@@ -66,6 +66,50 @@ class AuthService {
         newUserReference.setValue(["username": username, "usernameLowercase": username.lowercased(), "email": email, "profileImageUrl": profileImageUrl])
         onSuccess()
     }
+    
+    static func updateUserInfo (username: String, email: String,imageData: Data, onSuccess: @escaping () -> Void, onError: @escaping (_ errorMessage: String?) -> Void) {
+        Auth.auth().currentUser?.updateEmail(to: email, completion: { (error) in
+            if error != nil {
+                onError(error!.localizedDescription)
+            }
+        })
+        
+        let userId = Auth.auth().currentUser?.uid
+        let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOT_REF).child("profileImage").child(userId!)
+        storageRef.putData(imageData, metadata: nil, completion: { (metadata,  error) in
+            
+            if error != nil {
+                return
+            }
+            
+            storageRef.downloadURL(completion: { ( url, error ) in
+                
+                if error != nil {
+                    return
+                }
+                
+                if let profileImageUrl = url?.absoluteString {
+                    
+                    self.updateDatabase(profileImageUrl: profileImageUrl, username: username, email: email, onSuccess: onSuccess, onError: onError)
+                    
+                }
+            })
+        })
+        
+    }
+    
+    static func updateDatabase(profileImageUrl: String, username: String, email: String, onSuccess: @escaping () -> Void, onError: @escaping (_ errorMessage: String?) -> Void) {
+        let dict = ["username": username, "usernameLowercase": username.lowercased(), "email": email, "profileImageUrl": profileImageUrl]
+        Api.User.REF_CURRENT_USER?.updateChildValues(dict, withCompletionBlock: { (error, ref)in
+            if error != nil {
+               onError(error!.localizedDescription)
+            } else {
+                onSuccess()
+            }
+        })
+        
+    }
+    
     static func logout(onSuccess: @escaping () -> Void, onError: @escaping (_ errorMessage: String?) -> Void) {
         do {
             try Auth.auth().signOut()
--- Tromit/ViewControllers/SettingViewController.swift
+++ Tromit/ViewControllers/SettingViewController.swift
@@ -12,28 +12,54 @@ class SettingViewController: UITableViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Edit Profile"
         fetchCurrentUser ()
     }
+    
+    @IBAction func changeProfileButtonTapped(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        self.present(pickerController, animated: true, completion: nil)
+    }
+
+    
     func fetchCurrentUser() {
         Api.User.observeCurrentUser { (user) in
             self.usernameTextField.text = user.username
             self.emailTextField.text = user.email
             if let profileUrl = URL(string: user.profileImageUrl!){
-               self.profileImage.sd_setImage(with: profileUrl)
+               self.profileImageView.sd_setImage(with: profileUrl)
             }
         }
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
-        
+        if let profileImg = self.profileImageView.image, let imageData = profileImg.jpegData(compressionQuality: 0.1) {
+            AuthService.updateUserInfo(username: usernameTextField.text!, email: emailTextField.text!, imageData: imageData, onSuccess: {
+                ProgressHUD.showSuccess("Success")
+            }) { (errorMessage) in
+                ProgressHUD.showError(errorMessage)
+            }
+            
+        }
     }
     @IBAction func logoutButtonTapped(_ sender: Any) {
         
     }
     
+}
+extension SettingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        
+        if  let  image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
